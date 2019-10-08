18ad28d21dd49f6dc48d307a8002247040f581bc
uberpup <tochilin.vn@phystech.edu>
2019-10-08 14:55:31 +0300

    v.0.1 - fixed pointers, deque still unfixed

--- AhoCorasick/main.cpp
+++ AhoCorasick/main.cpp
@@ -31,7 +31,7 @@ public:
         std::weak_ptr<BohrNode> suff_link;
         bool is_terminal;  // Если терминальная, то храним индекс внутри словаря
         bool is_root;
-        size_t wordlist_index;
+        std::vector<size_t> wordlist_idxs;  // Можно сделать с-шным массивом, если будут проблемы по времени
 
         bool operator ==(const BohrNode& rhv);
         std::shared_ptr<BohrNode> FindTransition(char ch);
@@ -40,7 +40,7 @@ public:
     std::vector<std::pair<std::string, size_t>> wordlist;  // Строка и расстояние до начала
     // В случае вопросов в конце добавим фиктивную строку
     size_t pattern_size;
-    BohrNode root;
+    std::shared_ptr<BohrNode> root;
 
     Bohr();
     Bohr(size_t pattern_size);
@@ -60,13 +60,16 @@ int main() {
     size_t divider_count = 0;
     for (size_t i = 0; i < pattern.length(); ++i) {
         if (pattern[i] == DIVIDER) {  // Отдебажить бы
-            if (!last_char && last_char != i) {
-                bohr.AddPattern(pattern.substr(last_char, i), divider_count);
+            if (last_char != i) {
+                bohr.AddPattern(pattern.substr(last_char, i - last_char),
+                        divider_count);
                 divider_count = 0;
             }
             ++divider_count;
             last_char = i + 1;
-        } else {
+        } else if (i == pattern.length() - 1) {
+            bohr.AddPattern(pattern.substr(last_char, i - last_char + 1),
+                    divider_count);
             divider_count = 0;
         }
     }
@@ -86,20 +89,20 @@ int main() {
 }
 
 Bohr::BohrNode::BohrNode(): suff_link(), term_link(), transitions(),
-        is_terminal(false), is_root(false),
-        wordlist_index(-1) {}
+                            is_terminal(false), is_root(false),
+                            wordlist_idxs() {}
 
-Bohr::Bohr(): root(), wordlist(), pattern_size(0) {
-    root.suff_link = std::make_shared<BohrNode>(root);  // Спецификация полей под корень
-    root.term_link = root.suff_link;
-    root.is_root = true;
+Bohr::Bohr(): root(std::make_shared<BohrNode>()), wordlist(), pattern_size(0) {
+    root->suff_link = std::weak_ptr<BohrNode>(root);  // Спецификация полей под корень
+    root->term_link = std::weak_ptr<BohrNode>(root);
+    root->is_root = true;
 }
 
-Bohr::Bohr(size_t pattern_size): root(), wordlist(),
+Bohr::Bohr(size_t pattern_size): root(std::make_shared<BohrNode>()), wordlist(),
         pattern_size(pattern_size) {
-    root.suff_link = std::make_shared<BohrNode>(root);  // Спецификация полей под корень
-    root.term_link = root.suff_link;
-    root.is_root = true;
+    root->suff_link = std::weak_ptr<BohrNode>(root);  // Спецификация полей под корень
+    root->term_link = std::weak_ptr<BohrNode>(root);
+    root->is_root = true;
 }
 
 bool Bohr::BohrNode::operator ==(const Bohr::BohrNode& rhv) {  // На всякий случай
@@ -127,40 +130,39 @@ void Bohr::AddPattern(std::string pattern, size_t divider_count) {
 
     auto current_node = root;
     for (size_t i = 0; i < pattern.length(); ++i) {
-        auto neighbour_node = current_node.FindTransition(pattern[i]);
+        auto neighbour_node = current_node->FindTransition(pattern[i]);
         if (neighbour_node == nullptr) {
             neighbour_node = std::make_shared<BohrNode>();
-            if (current_node.is_root) {  // Первые символы ссылаются на корень
-                neighbour_node->suff_link = std::make_shared<BohrNode>(root);  // Does it work OK with weak_ptr?
+            if (current_node->is_root) {  // Первые символы ссылаются на корень
+                neighbour_node->suff_link = std::weak_ptr<BohrNode>(root);
                 neighbour_node->term_link = neighbour_node->suff_link;
             }
-            current_node.transitions[pattern[i]] = neighbour_node;
+            current_node->transitions[pattern[i]] = neighbour_node;
         }
-        current_node = *neighbour_node;
+        current_node = neighbour_node;
     }
-    if (!current_node.is_root) {
-        current_node.is_terminal = true;
-        current_node.wordlist_index = wordlist.size();
-        if (wordlist.size() > 0) {
-            wordlist.emplace_back(pattern,
-                                  wordlist.back().second + pattern.length() + divider_count);
-        } else {
-            wordlist.emplace_back(pattern, pattern.length() + divider_count);
-        }
+
+    if (!current_node->is_root) {
+        current_node->is_terminal = true;
+        current_node->wordlist_idxs.push_back(wordlist.size());
+        wordlist.emplace_back(pattern,
+                              (!wordlist.empty() ?
+                              wordlist.back().second + pattern.length() :
+                              pattern.length()) + divider_count);
     }
 }
 
 void Bohr::Init() {  // BFS
-    std::queue<std::shared_ptr<BohrNode>> bfs_queue;
-    bfs_queue.push(std::make_shared<BohrNode>(root));
+    std::queue<std::shared_ptr<BohrNode>> bfs_queue;  // Куда пропали переходы?
+    bfs_queue.push(root);
     while (!bfs_queue.empty()) {
-        auto& current_node = bfs_queue.front();
+        auto current_node = bfs_queue.front();
         bfs_queue.pop();
         for (auto& map_el : current_node->transitions) {  // Строим суффиксные и терминальные ссылки
             const char ch = map_el.first;
-            auto& neighbour_node = map_el.second;
+            auto neighbour_node = map_el.second;
 
-            auto& term_node = current_node->suff_link;
+            auto term_node = current_node->suff_link;
             while (!term_node.lock()->is_root) {  // Суффиксные
                 if (term_node.lock()->FindTransition(ch) != nullptr) {
                     neighbour_node->suff_link = term_node.lock()->FindTransition(ch);
@@ -170,7 +172,7 @@ void Bohr::Init() {  // BFS
                 }
             }
             if (neighbour_node->suff_link.lock() == nullptr) {
-                neighbour_node->suff_link = std::make_shared<BohrNode>(root);
+                neighbour_node->suff_link = std::weak_ptr<BohrNode>(root);
                 neighbour_node->term_link = neighbour_node->suff_link;
             }
 
@@ -187,39 +189,47 @@ void Bohr::Init() {  // BFS
 }
 
 void Bohr::Step(const char ch, std::shared_ptr<BohrNode>& current_node) {
-    while (current_node != nullptr && !(current_node->is_root)) {
+    while (current_node != nullptr) {
         auto candidate = current_node->FindTransition(ch);
         if (candidate != nullptr) {
             current_node = candidate;
             return;
         }
+        if (candidate == nullptr && current_node == root) {
+            return;
+        }
         current_node = current_node->suff_link.lock();
     }
-    if (!(current_node->is_root)) {
-        current_node = std::make_shared<BohrNode>(root);
+
+    if (!(current_node->is_root)) {  // Если никого не нашли
+        current_node = root;
     }
 }
 
-std::vector<size_t> Bohr::PatternSearch(const std::string& text) {  // Внимательно всмотреться в работу с вопросиками
+std::vector<size_t> Bohr::PatternSearch(const std::string& text) {  // Проверка вопросов в конце
     std::vector<size_t> pattern_indexes;
     std::deque<size_t> search_deque(pattern_size);
     size_t patterns_number = wordlist.size();
 
-    auto current_node = std::make_shared<BohrNode>(root);
+    auto current_node = root;
     for (size_t i = 0; i < text.length(); ++i) {
         Step(text[i], current_node);
+        auto additional_node = current_node;
+        while (!additional_node->is_root) {
+            if (additional_node->is_terminal) {
+                for (const auto& idx : additional_node->wordlist_idxs) {
+                    // Вычитаем из размера расстояние до предполагаемого начала слова
+                    ++search_deque[search_deque.size() - wordlist[idx].second];
+                }
+            }
+            additional_node = additional_node->term_link.lock();
+        }
 
         if (search_deque.front() == patterns_number) {  // Все слова из куска встретились
             pattern_indexes.push_back(i);
         }
         search_deque.pop_front();
         search_deque.push_back(0);
-
-        while (current_node->is_terminal) {
-            size_t dist = wordlist[current_node->wordlist_index].second;  // Расстояние до предполагаемого начала слова
-            ++search_deque[search_deque.size() - dist];
-            current_node = current_node->term_link.lock();
-        }
 
     }
     return pattern_indexes;
