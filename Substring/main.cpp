33be7dc831c0f055aed41ac655c17f83cf41e23d
uberpup <tochilin.vn@phystech.edu>
2019-10-09 05:00:13 +0300

    Substring fixed

--- Substring/main.cpp
+++ Substring/main.cpp
@@ -11,39 +11,58 @@
 #include <string>
 #include <vector>
 
-const char SPECIAL_SYMBOL = '#';  // Unused as a symbol in pattern/text
+std::vector<size_t> OnlineOccurrenceIdx(const std::string& pattern_with_symbol);
 
-std::vector<size_t> OccurrenceIdx(size_t pattern_length,
-        const std::string& text);
+size_t PrefixFunction(const std::string& pattern_with_symbol, char current_char,
+                      const std::vector<size_t>& prefix_function_results, size_t prev_value);
 
 std::vector<size_t> PrefixFunction(const std::string& text);
 
 int main() {
-    std::string pattern, text;
+    std::string pattern;
 
     std::cin >> pattern;
-    std::cin >> text;
-    text = pattern + SPECIAL_SYMBOL + text;  // Adding pattern and symbol
-    auto result = OccurrenceIdx(pattern.size(), text);
+    auto result = OnlineOccurrenceIdx(pattern);
     for (const auto& idx : result){
         std::cout << idx << " ";
     }
     return 0;
 }
 
-std::vector<size_t> OccurrenceIdx(size_t pattern_length,
-        const std::string& text) {
-    std::vector<size_t> prefix_function_result = PrefixFunction(text);
-    std::vector<size_t> result;
-    for (size_t i = pattern_length; i < text.size(); ++i) {
-        if (prefix_function_result[i] == pattern_length) {  // Substr found
-            result.push_back(i - 2 * pattern_length);  // Fixing the indexes
+std::vector<size_t> OnlineOccurrenceIdx(const std::string& pattern) {
+    char in_char;
+    auto prefix_function_results = PrefixFunction(pattern);
+    std::vector<size_t> occurrence_idxes;
+
+    size_t prev_value = 0;
+    size_t counter = pattern.length();
+    while (std::cin >> in_char) {
+        prev_value = PrefixFunction(pattern,
+                                    in_char,
+                                    prefix_function_results, prev_value);
+        if (prev_value ==
+                pattern.length()) {
+            occurrence_idxes.push_back(counter - 2 * pattern.length() + 1);
         }
+        ++counter;
     }
-    return result;
+    return occurrence_idxes;
 }
 
-std::vector<size_t> PrefixFunction(const std::string& text) {  // O(n)
+size_t PrefixFunction(const std::string& pattern_with_symbol, char current_char,
+        const std::vector<size_t>& prefix_function_results, size_t prev_value) {  // Преф. функция текста
+    auto prefix_length = prev_value;
+    while (prefix_length > 0 && current_char !=
+        pattern_with_symbol[prefix_length]) {
+        prefix_length = prefix_function_results[prefix_length - 1];
+    }
+    if (pattern_with_symbol[prefix_length] == current_char) {
+        return ++prefix_length;
+    }
+    return prefix_length;
+}
+
+std::vector<size_t> PrefixFunction(const std::string& text) {  // O(n), для обработки паттерна
     std::vector<size_t> prefix_function_result(text.size());
     prefix_function_result[0] = 0;
     int prefix_length;
